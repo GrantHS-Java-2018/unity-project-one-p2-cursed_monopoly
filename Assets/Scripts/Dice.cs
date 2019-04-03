9d7b1f8ebdc9de048dc41d0b131da5d0461c9956
Kirk <kjid.dibert@gmail.com>
2019-04-03 11:35:30 -0700

    first thing
    first one

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class Dice : MonoBehaviour
+{   
+    [SerializeField]private Sprite[] dieFace;
+
+    public int value;
+    // Start is called before the first frame update
+    void Awake ()
+    {
+
+    }
+    void Start()
+    {
+        StartCoroutine(Roll());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Roll()
+    {
+        var randNum = new Random();
+        int num =randNum.Next(1, 7);
+        GetComponent<SpriteRenderer>().sprite = dieFace[num - 1];
+        value = num;
+        yield return new WaitForSeconds(.15f);
+        StartCoroutine(Roll());
+    }
+}
